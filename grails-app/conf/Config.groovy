11e1f2937fbbad61ae9cc0f57bff0e7286e1b28a
Vahid Hedayati <badvad@gmail.com>
2019-04-08 19:29:35 +0100

    0.14 release

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -116,18 +116,57 @@ log4j.main = {
            'net.sf.ehcache.hibernate'
 }
 //New Way Controller supports host from config file
+/*
 remotessh.HOST = "localhost"
 remotessh.USER = "mx1"
 remotessh.PASS=""
 remotessh.KEY="/home/mx1/.ssh/id_rsa"
 remotessh.KEYPASS=""
 remotessh.PORT="22"
-
+*/
+remotessh {
+	HOST = "localhost"
+	USER = "mx1"
+	//PASS=""
+	KEY="/home/mx1/.ssh/id_rsa"
+	KEYPASS=""
+	PORT="22"
+	
+	/*
+	 * keepAliveTime in seconds
+	 */
+	keepAliveTime=300
+	
+	/*
+	 * corePoolSize this should match maximumPoolSize
+	 *
+	 */
+	corePoolSize=3
+	/*
+	 * maxPoolSize
+	 *
+	 */
+	maximumPoolSize=3
+	
+	/*
+	 * Amount of elements that can queue
+	 */
+	maxQueue=100
+	
+}
 //0.11 new customised config values can be defined 
 
 mySshConfigVar.HOST = "localhost"
 mySshConfigVar.USER = "mx1"
-mySshConfigVar.PASS="somePass"
+mySshConfigVar.PASS="password"
 //mySshConfigVar.KEY="/home/mx1/.ssh/id_rsa"
 //mySshConfigVar.KEYPASS=""
 mySshConfigVar.PORT="22"
+
+
+mySshConfigVar2.HOST = "localhost"
+mySshConfigVar2.USER = "mx1"
+mySshConfigVar2.PASS="password"
+//mySshConfigVar.KEY="/home/mx1/.ssh/id_rsa"
+//mySshConfigVar.KEYPASS=""
+mySshConfigVar2.PORT="22"
